ed34e26f538ec3dd8cb247e2da501005b03fd722
Danuta Cybulska <danuta.cybulska.biz@gmail.com>
2022-12-12 12:20:11 +0100

    Create data base images

---
+++ js/positions.js
@@ -0,0 +1,100 @@
+const positions = [
+    {
+        id:'1',
+        title:'fhyavbli',
+        company:'Photo One',
+        image: 'images/diyar-shahbaz-C58W1qoE0Bg-unsplash.png',
+        price: 8.23
+    },
+    {
+        id:'2',
+        title:'fhyavbli 2',
+        company:'Photo One',
+        image: 'images/diyar-shahbaz-FHk5TS4A7yU-unsplash.png',
+        price: 5.23
+    },
+    {
+        id:'3',
+        title:'fhyavbli 3',
+        company:'Photo One',
+        image: 'images/diyar-shahbaz-gJJdY4cUIKA-unsplash.png',
+        price: 8.45
+    },
+    {
+        id:'4',
+        title:'fhyavbli 4',
+        company:'Photo One',
+        image: 'images/diyar-shahbaz-Xxz97xNbdHs-unsplash.png',
+        price: 7.45
+    },
+    {
+        id:'5',
+        title:'fhyavbli 5',
+        company:'Photo Two',
+        image: 'images/mario-von-rotz-9st5A-3dWP4-unsplash.png',
+        price: 5.45
+    },
+    {
+        id:'6',
+        title:'fhyavbli 6',
+        company:'Photo Two',
+        image: 'images/mario-von-rotz-HW-H5kvPL9Y-unsplash.png',
+        price: 5.45
+    },
+    {
+        id:'7',
+        title:'fhyavbli 7',
+        company:'Photo Two',
+        image: 'images/mario-von-rotz-Pfn1JsRM_v4-unsplash.png',
+        price: 5.45
+    },
+    {
+        id:'8',
+        title:'fhyavbli 8',
+        company:'Photo Two',
+        image: 'images/mario-von-rotz-sgaA4_eyL3s-unsplash.png',
+        price: 5.45
+    },
+    {
+        id:'9',
+        title:'fhyavbli 9',
+        company:'Photo Two',
+        image: 'images/mario-von-rotz-WPt_8gv0UsA-unsplash.png',
+        price: 5.45
+    },
+    {
+        id:'10',
+        title:'fhyavbli 10',
+        company:'Photo Two',
+        image: 'images/mario-von-rotz-zixE_0h5kgc-unsplash.png',
+        price: 5.45
+    },
+    {
+        id:'11',
+        title:'fhyavbli 11',
+        company:'Photo Ziko',
+        image: 'images/maxim-berg-cr6u8XFA0-k-unsplash.png',
+        price: 5.45
+    },
+    {
+        id:'12',
+        title:'fhyavbli 12',
+        company:'Photo Ziko',
+        image: 'images/maxim-berg-fKPZGJI4dXU-unsplash.png',
+        price: 5.45
+    },
+    {
+        id:'13',
+        title:'fhyavbli 13',
+        company:'Photo Ziko',
+        image: 'images/maxim-berg-JiM_eFf2O3M-unsplash.png',
+        price: 5.45
+    },
+    {
+        id:'14',
+        title:'fhyavbli 14',
+        company:'Photo Ziko',
+        image: 'images/maxim-berg-TcE45yIzJA0-unsplash.png',
+        price: 5.45
+    }
+];
